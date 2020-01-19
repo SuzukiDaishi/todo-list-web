a85ce5c7820603952fe747a9969c914d672bc1f2
SuzukiDaishi <34536327+SuzukiDaishi@users.noreply.github.com>
2020-01-19 10:57:18 +0900

    コンテナ作成中

--- todo-app/pages/index.vue
+++ todo-app/pages/index.vue
@@ -2,9 +2,16 @@
   <div class="wrapper">
     <header>
       <div id="header-title">
-        <h1>TODO LIST</h1>
+        <h1>ToDo List</h1>
       </div>
     </header>
+    <main class=".container">
+      <div id="header-space"></div>
+      <div class="item">Item 1</div>
+      <div class="item">Item 2</div>
+      <div class="item">Item 3</div>
+      <div class="item">Item 4</div>
+    </main>
   </div>
 </template>
 
@@ -14,7 +21,20 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300&display=swap');
+
+.wrapper {
+  background: #222222;
+  width: 100vw;
+  height: 100vh;
+}
+
+html {
+  overscroll-behavior: none;
+}
+
 header {
+  position:fixed;
   width: 100vw;
   max-height: 300px;
   height: 20vh;
@@ -29,14 +49,32 @@ header>#header-title {
   transform-origin: top left;
 }
 header>#header-title>h1 {
-  padding: 2vw;
-  font-size: 3em;
+  margin: 2vw;
+  font-size: 3.5em;
   color: white;
   text-shadow: black 0 0 5px;
+  font-family: 'Open Sans Condensed', sans-serif;
 }
-.wrapper {
-  background: #222222;
-  width: 100vw;
-  height: 100vh;
+
+main {
+  background: #f00;
+  overflow-y: scroll;
+  
 }
+
+#header-space {
+  max-height: 300px;
+  height: 20vh;
+}
+.container {
+  display: flex;
+}
+.item {
+  margin: 0 auto;
+  margin-top: 1vh;
+  width: 80vw;
+  height: 20vh;
+  background: #00f;
+}
+
 </style>
